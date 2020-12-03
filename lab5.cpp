99c4a3acb5f6ed3bf198258310d1038e24ea65ed
Fixer1 <figat.wcale.nie@gmail.com>
2020-12-03 13:08:17 +0000

    solve

--- lab5.cpp
+++ lab5.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
-
+#include <vector>
 int main()
 {
-    puts("Hello, World!");
+    std::vector< int >           v{1, 2, 3};
+    std::vector< int >::iterator it = v.begin();
+
+    // Jawnie drukujemy zawartość wektora
+    std::cout << *it++;
+    std::cout << *it++;
+    std::cout << *it;
+    std::vector< std::string > vs;             // pusty wektor stringów
+    std::string                wyraz{"słowo"}; // przykładowy string
+    vs.push_back(wyraz);                       // dodaj kopię istniejącego stringa do wektora
+    wyraz += " drugie słowo";                  // obiekt wyraz istnieje niezależnie
+    vs.emplace_back("trzecie słowo");
+    
 }
--- zal/foo.hpp
+++ zal/foo.hpp
@@ -7,6 +7,15 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+   std::vector < char> char_vector (people.size(), 'n');
+   int j = 0;
+  for (std::list<Human>::iterator it = people.begin(); it != people.end(); std::advance(it, 1)) {
+  *it.birthday();
+  
+  if (*it.isMonster){
+    char_vector[people.size()-j-1]='y'
+      }
+  j++;
+}}
+    return char_vector;
 }
