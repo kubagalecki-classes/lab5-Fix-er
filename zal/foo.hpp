aba84ddab4b64d404d9edceffd6bfe6be75f9ee2
Fixer1 <figat.wcale.nie@gmail.com>
2020-12-03 14:08:11 +0000

    s3

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -7,13 +7,13 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-   std::vector < char> char_vector (people.size(), 'n');
+   std::vector < char> char_vector (people.size(), 'y');
    int j = 0;
   for (std::list<Human>::iterator it = people.begin(); it != people.end(); std::advance(it, 1)) {
   it->birthday();
   
   if (it->isMonster()){
-    char_vector[people.size()-j-1]='y';
+    char_vector[people.size()-j-1]='n';
       }
   j++;
 }
